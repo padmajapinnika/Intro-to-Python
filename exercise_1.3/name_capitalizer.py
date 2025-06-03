87749b20f9e60ecffbe0fe674a447b6656b82210
padmajapinnika <padmajapinnika@gmail.com>
2025-06-03 17:26:05 -0400

    Add recipe input script with difficulty calculation and ingredient tracking

---
+++ exercise_1.3/name_capitalizer.py
@@ -0,0 +1,9 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+first_name = first_name.capitalize()
+last_name = last_name.capitalize()
+print("Your name is", first_name, last_name)
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number to be added to the first: "))
+print("The sum of these numbers is " + str(a + b))
